8f109f78573a4630cfaece25a7da923c174e7793
Sam Fourie <sfourie@jhb.dvt.co.za>
2022-08-05 09:50:14 +0200

    Initial Commit - Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-red-900'>
+      <h1 className='text-xl'>Hello Sexy Potatoes</h1>
+      </div>
+  )
+}
+
+export default App
